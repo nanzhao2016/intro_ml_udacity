475eeca5bfe4436242a37969628941315d96d469
nanzhao2016 <bella.zn@gmail.com>
2017-03-29 10:36:15 +0200

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,54 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.metrics import accuracy_score
+comp = {}
+
+"""
+gnb = GaussianNB()
+t0 = time()
+gnb.fit(features_train, labels_train)
+print("training time: ", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = gnb.predict(features_test)
+print("predicting time: ", round(time()-t1, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(pred, labels_test)
+
+print(accuracy)
+"""
+gnb = GaussianNB()
+gnb.fit(features_train, labels_train)
+pred = gnb.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+comp['gnb'] = accuracy
+
+mnb = MultinomialNB()
+mnb.fit(features_train, labels_train)
+pred = mnb.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+comp['mnb'] = accuracy
+
+bnb = BernoulliNB()
+bnb.fit(features_train, labels_train)
+pred = bnb.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+comp['bnb'] = accuracy
+
+import pprint
+
+pprint.pprint(comp)
+
+
 
 
 #########################################################
+
+
+
 
 
