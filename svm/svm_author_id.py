859255feafa2065124671683c551ddd22c82c610
nanzhao2016 <bella.zn@gmail.com>
2017-03-29 10:37:46 +0200

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,54 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import pprint
+
+results = {}
+#sample = round(len(features_train)/100)
+sample = len(features_train)
+features_train = features_train[: sample]
+labels_train = labels_train[: sample]
+
+"""
+svc = svm.SVC(kernel = "linear")
+t0 = time()
+svc.fit(features_train, labels_train)
+print("training time is: ", time()-t0, "s")
+pred = svc.predict(features_test)
+
+accuracy = accuracy_score(pred, labels_test)
+results["linear"] = accuracy
+
+svc = svm.SVC(kernel = "rbf")
+t0 = time()
+svc.fit(features_train, labels_train)
+print("training time is: ", time()-t0, "s")
+pred = svc.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+results["rbf"] = accuracy
+
+pprint.pprint(results)
+"""
+"""
+c = [1, 10, 100, 1000, 10000]
+for i in c:
+    svc = svm.SVC(kernel = "rbf", C=i)
+    svc.fit(features_train, labels_train)
+    pred = svc.predict(features_test)
+    accuracy = accuracy_score(pred, labels_test)
+    results[str(i)] = accuracy	
+
+pprint.pprint(results)
+"""
+svc = svm.SVC(kernel = "rbf", C=10000)
+svc.fit(features_train, labels_train)
+pred = svc.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)	
+
+print(sum(pred))
+
+
 
